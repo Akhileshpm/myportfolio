0db022cb38ebeac49952288e25383c918c27c307
Akhileshpm <appumithr@gmail.com>
2022-11-26 15:15:55 +0530

    add scroll horizontal to skills

--- src/components/Skillset.js
+++ src/components/Skillset.js
@@ -8,26 +8,38 @@ import python from '../img/python.png';
 import react from '../img/react.png';
 import cpp from '../img/cpp.png';
 import '../styles/skillset.css';
-    function reveal() {
-        var reveals = document.querySelectorAll("#skillset-heading");
-        for (var i = 0; i < reveals.length; i++) {
-          var windowHeight = window.innerHeight;
-          var elementTop = reveals[i].getBoundingClientRect().top;
-          var elementVisible = 150;
-          
-          if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-          } else {
-            reveals[i].classList.remove("active");
-          }
-        }
+
+function reveal() {
+    var reveals = document.querySelectorAll("#skillset-heading");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
       }
+    }
+    const buttonLeft = document.getElementById("left-btn");
+    const buttonRight = document.getElementById("right-btn");
+
+    buttonRight.onclick = function () {
+      document.getElementById('skillset-box').scrollLeft += 350;
+    };
+    buttonLeft.onclick = function () {
+      document.getElementById('skillset-box').scrollLeft -= 350;
+    };
+}
+
 const Skillset = () => {
 
     window.addEventListener("scroll", reveal);
     return (
         <div className='skillSet'>
             <div id="skillset-heading"> Professional Skillset</div>
+            <div id='left-btn'>🢀</div>            
             <div id="skillset-box">
                 <SkillTag imgName={mongoDB}/>
                 <SkillTag imgName={postgres}/>
@@ -37,8 +49,11 @@ const Skillset = () => {
                 <SkillTag imgName={react}/>            
                 <SkillTag imgName={python}/>
             </div>
+            <div id='right-btn'>🢂</div>            
         </div>
     );
 };
+
+
 
 export default Skillset;
